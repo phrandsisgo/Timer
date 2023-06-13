fbabe7803088d4ba6642d191bb2618168351cc57
Francisco Wohlgemuth <francisco.wohlgemuth@hotmail.com>
2023-06-13 16:59:26 +0200

    timer almost done

--- stopScript.js
+++ stopScript.js
@@ -1,4 +1,6 @@
-console.log('stopScript.js is beeing accessed');
+//I need to declare a global variable for the widgetId
+let widgetIdNumber = 0;
+
 function createDialogForNewStopWatch(){
     console.log('createDialogForNewStopWatch()');
     let mainDialog = document.createElement('div');
@@ -24,6 +26,7 @@ function createDialogForNewStopWatch(){
 
     let inputHour = document.createElement('input');
     inputHour.type = 'number';
+    inputHour.classList.add('inputField', 'col-1');
     inputHour.id = 'inputHour';
     flexdDiv.appendChild(inputHour);
 
@@ -33,7 +36,8 @@ function createDialogForNewStopWatch(){
 
     let inputMinute = document.createElement('input');
     inputMinute.type = 'number';
-    inputMinute.id = 'inputMinute';
+    inputMinute.id = 'inputMinute', 'col-1';
+    inputMinute.classList.add('inputField', 'col-1');
     flexdDiv.appendChild(inputMinute);
 
     let labelSecond = document.createElement('label');
@@ -43,6 +47,7 @@ function createDialogForNewStopWatch(){
     let inputSecond = document.createElement('input');
     inputSecond.type = 'number';
     inputSecond.id = 'inputSecond';
+    inputSecond.classList.add('inputField', 'col-1');
     flexdDiv.appendChild(inputSecond);
 
     //what I have as inputs are so far: inputHour, inputMinute, inputSecond
@@ -63,42 +68,138 @@ function createDialogForNewStopWatch(){
 
 }
 
-function createStopWatch(hours,minutes,seconds){
-    
-    console.log("creaate Stop watch"+ hours +"h" + minutes + "m" + seconds + "s");
 
+function createStopWatch(hours,minutes,seconds,widgetId){
+    let widgetIdString = 'sw' + widgetIdNumber.toString(); //sw = abkürzung für "stop watch"
+    let combinedTime = hours + minutes + seconds;
+    
     let stopWatch = document.createElement('div');
-    stopWatch.class = 'Time-card row';
+    stopWatch.classList.add('bg-color-success', 'Time-card', 'row');
+    stopWatch.id = widgetIdString;
     document.body.appendChild(stopWatch);
 
-    let Zeile1 = document.createElement('div');
-    Zeile1.class = 'Zeile1 text-center col-12';
-    stopWatch.appendChild(Zeile1);
+    let firstLineWidget = document.createElement('div');
+    firstLineWidget.classList.add('col-12', 'Zeile1', 'text-center');
+    stopWatch.appendChild(firstLineWidget);
 
-    let abstand1 = document.createElement('div');
-    abstand1.class = 'col-1';
-    Zeile1.appendChild(abstand1);
+    let spacerdiv = document.createElement('div');
+    spacerdiv.classList.add('col-1');
+    firstLineWidget.appendChild(spacerdiv);
 
-    let zeitAnzeige = document.createElement('h4');
-    zeitAnzeige.class = 'col-5 fontDigitalNumber';
-    zeitAnzeige.innerHTML = hours + ":" + minutes + ":" + seconds;
-    Zeile1.appendChild(zeitAnzeige);
+    //Für die Anzeige braucht es noch eine Funktion
+    let digitalAnzeige = document.createElement('h4');
+    digitalAnzeige.id = widgetIdString + 'digitalAnzeige';
+    digitalAnzeige.classList.add('fontDigitalNumber', 'col-5');
+    digitalAnzeige.innerHTML =  "Zeit sollte hier eingefüllt werden";
+    digitalAnzeige.setAttribute('functionId', widgetIdString + 'digitalAnzeige');
+    //if I want to access it by this attribute I need to use digitalAnzeige.getAttribute('data-*');
+    firstLineWidget.appendChild(digitalAnzeige);
+
+    firstLineWidget.appendChild(spacerdiv);
+
+    let trashIcon = document.createElement('img');
+    let fritz = 12;
+    trashIcon.src = 'trash1.svg';
+    trashIcon.classList.add('col-4','icon');
+    trashIcon.alt = 'trashIcon';
+    trashIcon.addEventListener('click',function(){
+        document.getElementById(widgetIdString).remove();
+    });
+    firstLineWidget.appendChild(trashIcon);
+
+
+    //ende der ersten Zeile
+
+    let secondLineWidget = document.createElement('div');
+    secondLineWidget.classList.add('col-12', 'Zeile2', 'text-center');
+    stopWatch.appendChild(secondLineWidget);
+
+    let spacerdiv2 = document.createElement('div');
+    spacerdiv2.classList.add('col-3');
+    secondLineWidget.appendChild(spacerdiv2);
+
+    let playIcon = document.createElement('img');
+    playIcon.src = 'play.svg';
+    playIcon.classList.add('col-3', 'icon');
+    playIcon.alt = 'playIcon';
+    playIcon.setAttribute('functionId', widgetIdString + 'digitalAnzeige');
+    playIcon.addEventListener('click',function(){
+        playTimer(widgetIdString + 'digitalAnzeige');
+    });
+    //beispielsZeit(); muss noch programmiert werden
+    //function targetTimer(Stunden, Minuten, Sekunden, anzeigeString){
+    secondLineWidget.appendChild(playIcon);
+
+    let pauseIcon = document.createElement('img');
+    pauseIcon.src = 'pause.svg';
+    pauseIcon.classList.add('col-3', 'icon');
+    pauseIcon.alt = 'pauseIcon';
+    pauseIcon.setAttribute('functionId', widgetIdString + 'digitalAnzeige');
+    pauseIcon.addEventListener('click',function(){
+        pauseTimer(widgetIdString + 'digitalAnzeige');
+    });
+    //pauseIcon.onclick = 'pauseCountdown()';
+    secondLineWidget.appendChild(pauseIcon);
+
+    let resetIcon = document.createElement('img');
+    resetIcon.src = 'reset.svg';
+    resetIcon.classList.add('col-3', 'icon');
+    resetIcon.alt = 'resetIcon';
+    resetIcon.setAttribute('functionId', widgetIdString + 'digitalAnzeige');
+    resetIcon.setAttribute('beginningTimer', combinedTime);
+    resetIcon.addEventListener('click',function(){
+        resetTimer(widgetIdString + 'digitalAnzeige', combinedTime);
+    });
+    //resetIcon.onclick = 'resetCountdown()';
+    secondLineWidget.appendChild(resetIcon);
+
+    let intervalId;
+
+    targetTimer(hours, minutes, seconds, widgetIdString + 'digitalAnzeige')//calling the timer function that sets a interval to the timer
+
+    function targetTimer(Stunden, Minuten, Sekunden, anzeigeStringId){
+
+        let distanceFinal = Stunden*60*60*1000 + Minuten*60*1000 + Sekunden*1000;
+        intervalId = setInterval(function() {
+            distanceFinal = distanceFinal - 1000;
+            
+            var stunden = Math.floor((distanceFinal % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minuten = Math.floor((distanceFinal % (1000 * 60 * 60)) / (1000 * 60));
+            var sekunden = Math.floor((distanceFinal % (1000 * 60)) / 1000);
+            document.getElementById(anzeigeStringId).innerHTML = stunden + "h " + minuten + "m " + sekunden + "s ";
+            if (distanceFinal < 0) {
+                clearInterval(z);
+                document.getElementById(anzeigeStringId).innerHTML = "EXPIRED";
+            }
+    },1000);
+    }
+    function pauseTimer(anzeigeStringId){
+        clearInterval(intervalId);
+    }
+    function resetTimer(anzeigeStringId, beginningTimer){
+        clearInterval(intervalId);
+        targetTimer(beginningTimer);
+    }
+    function playTimer(anzeigeStringId){
+        //targetTimer();
+    }
+    //update the variable
+    widgetIdNumber++;
+  
 }
 
-var x;
 
 function beispielsZeit(){
-        let minutes = 3;
-        let seconds = 12;
+    let minutes = 3;
+    let seconds = 12;
         
-        let distanceFinal=  minutes*60*1000+seconds*1000;
+    var x;
+    let distanceFinal=  minutes*60*1000+seconds*1000;
         x = setInterval(function() {
         distanceFinal = distanceFinal - 1000;
-        console.log(distanceFinal+" should be distanceFinal");
 
         var minuten = Math.floor((distanceFinal % (1000 * 60 * 60)) / (1000 * 60));
         var sekunden = Math.floor((distanceFinal % (1000 * 60)) / 1000);
-        console.log(minuten+"m "+sekunden+"s")
         document.getElementById("digitaAnzeige").innerHTML = minuten + "m " + sekunden + "s ";
         if (distanceFinal < 0) {
             clearInterval(x);
@@ -106,8 +207,27 @@ function beispielsZeit(){
         }
     }, 1000);
 }
+/*
+function targetTimer(Stunden, Minuten, Sekunden, anzeigeStringId){
+    let distanceFinal = Stunden*60*60*1000 + Minuten*60*1000 + Sekunden*1000;
+    let z = setInterval(function() {
+        distanceFinal = distanceFinal - 1000;
+        
+        var stunden = Math.floor((distanceFinal % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minuten = Math.floor((distanceFinal % (1000 * 60 * 60)) / (1000 * 60));
+        var sekunden = Math.floor((distanceFinal % (1000 * 60)) / 1000);
+        document.getElementById(anzeigeStringId).innerHTML = stunden + "h " + minuten + "m " + sekunden + "s ";
+        if (distanceFinal < 0) {
+            clearInterval(z);
+            document.getElementById(anzeigeStringId).innerHTML = "EXPIRED";
+        }
+},1000);
+
+}*/
+function deleteCountdown(){
+//code kommt hier hin
+}
 function stopCountdown(){
-    console.log("stopcountdown called");
     clearInterval(x);
     document.getElementById("beispiel1").remove();
 }
